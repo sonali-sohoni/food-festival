fb0e771fbc77292064c1b9502f26fd780ebfec4e
sonali_sohoni <ssohoni@camsbycbs.com>
2022-01-04 13:03:24 -0500

    webpacks

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { fileURLToPath } = require("url");
+const webpack = require("webpack");
 
 module.exports = {
 	entry: "./assets/js/script.js",
@@ -7,5 +8,11 @@ module.exports = {
 		path: path.resolve(__dirname, "dist"),
 		filename: "main.bundle.js",
 	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+		}),
+	],
 	mode: "development",
 };
